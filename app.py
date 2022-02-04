4743de777f3061954d52faed7634c277d8ee76a0
vcm <sophierubin1224@gmail.com>
2022-02-04 02:28:56 -0500

    second commit

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from dash import *
 from testapp import *
+from testapp.github_info_header import github_info_header
+
 app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
-    #github_info_header(),
+    github_info_header(),
     html.Img(src="assets/cute_ferret.jpeg")
 ])
 
