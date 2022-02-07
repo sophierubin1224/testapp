628950d9ee549dbdcc494ad02801807832c6c737
angoble01 <ang47@duke.edu>
2022-02-07 12:45:40 -0500

    server set up 2

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
-cd C:\Users\vcm\Desktop\testapp
+cd C:\Users\vcm\PycharmProjects\testapp
 git pull https://%TESTAPP_GIT_PAT%@github.com/angoble01/testapp.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
