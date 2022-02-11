3200b7fc6764315e866e226d0f979fa605025d82
Xuanting67 <>
2022-02-11 18:21:07 -0500

    mario2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from testapp import *
 app = Dash(__name__)
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/tvol_banner.png")
+    html.Img(src="assets/mario2.jpeg")
 ])
 
 if __name__ == '__main__':
